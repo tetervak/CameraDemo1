70452c92056855bf4ba0ad02869b148b830250e0
Alex Tetervak <tetervak@sheridancollege.ca>
2019-12-04 22:53:01 -0500

    refactored context in PictureFragment

--- app/src/main/java/ca/javateacher/camerademo1/PictureFragment.java
+++ app/src/main/java/ca/javateacher/camerademo1/PictureFragment.java
@@ -1,48 +1,36 @@
 package ca.javateacher.camerademo1;
 
-
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
-import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.core.content.FileProvider;
 import androidx.fragment.app.Fragment;
-
-import android.provider.ContactsContract;
 import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
 import android.widget.ImageView;
-
 import java.io.File;
-import java.util.Date;
 import java.util.List;
 
-
-/**
- * A simple {@link Fragment} subclass.
- */
 public class PictureFragment extends Fragment {
 
   private static final int REQUEST_PHOTO = 2;
-
   private static final String mFileName = "picture";
   private File mPhotoFile;
-
-  private ImageButton mCameraButton;
   private ImageView mPhotoView;
+  private Context mAppContext;
 
   public PictureFragment() {
     // Required empty public constructor
   }
-
 
   @Override
   public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -50,36 +38,39 @@ public class PictureFragment extends Fragment {
     // Inflate the layout for this fragment
     View view = inflater.inflate(R.layout.fragment_picture, container, false);
 
-    File filesDir = getContext().getFilesDir();
+    mAppContext = view.getContext().getApplicationContext();
+
+    File filesDir = mAppContext.getFilesDir();
     mPhotoFile = new File(filesDir, mFileName);
 
-    PackageManager packageManager = getActivity().getPackageManager();
+    PackageManager packageManager = mAppContext.getPackageManager();
 
-    mCameraButton = view.findViewById(R.id.camera_button);
+    ImageButton cameraButton = view.findViewById(R.id.camera_button);
     final Intent captureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
     boolean canTakePhoto = mPhotoFile != null &&
       captureIntent.resolveActivity(packageManager) != null;
-    mCameraButton.setEnabled(canTakePhoto);
+    cameraButton.setEnabled(canTakePhoto);
 
-    mCameraButton.setOnClickListener(v -> getPicture(captureIntent));
+    cameraButton.setOnClickListener(v -> getPicture(captureIntent));
 
     mPhotoView = view.findViewById(R.id.photo_view);
+
     updatePhotoView();
 
     return view;
   }
 
   private void getPicture(Intent captureIntent) {
-    Uri uri = FileProvider.getUriForFile(getActivity(),
+    Uri uri = FileProvider.getUriForFile(mAppContext,
       "ca.javateacher.camerademo1.fileprovider", mPhotoFile);
     captureIntent.putExtra(MediaStore.EXTRA_OUTPUT, uri);
 
-    List<ResolveInfo> cameraActivities = getActivity()
-      .getPackageManager().queryIntentActivities(captureIntent,
+    List<ResolveInfo> cameraActivities
+      = mAppContext.getPackageManager().queryIntentActivities(captureIntent,
         PackageManager.MATCH_DEFAULT_ONLY);
 
     for (ResolveInfo activity : cameraActivities) {
-      getActivity().grantUriPermission(activity.activityInfo.packageName,
+      mAppContext.grantUriPermission(activity.activityInfo.packageName,
         uri, Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
     }
 
@@ -89,12 +80,13 @@ public class PictureFragment extends Fragment {
   private void updatePhotoView() {
     if (mPhotoFile == null || !mPhotoFile.exists()) {
       mPhotoView.setImageDrawable(null);
-      mPhotoView.setContentDescription("no image");
+      mPhotoView.setContentDescription(
+        mAppContext.getString(R.string.no_image_captured));
     } else {
-      Bitmap bitmap = PictureUtils.getScaledBitmap(
-        mPhotoFile.getPath(), getActivity());
+      Bitmap bitmap = PictureUtils.getScaledBitmap(mPhotoFile.getPath(), getActivity());
       mPhotoView.setImageBitmap(bitmap);
-      mPhotoView.setContentDescription("captured image");
+      mPhotoView.setContentDescription(
+        mAppContext.getString(R.string.captured_image));
     }
   }
 
@@ -102,10 +94,10 @@ public class PictureFragment extends Fragment {
   public void onActivityResult(int requestCode, int resultCode, Intent data) {
 
    if (resultCode == Activity.RESULT_OK && requestCode == REQUEST_PHOTO) {
-      Uri uri = FileProvider.getUriForFile(getActivity(),
+      Uri uri = FileProvider.getUriForFile(mAppContext,
         "ca.javateacher.camerademo1.fileprovider", mPhotoFile);
 
-      getActivity().revokeUriPermission(uri, Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
+      mAppContext.revokeUriPermission(uri, Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
 
       updatePhotoView();
     }
